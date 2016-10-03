dcecd3a69fd358cae5aeb3f1c7f2457c079a6558
Adam Crabtree <dude@noderiety.com>
2016-10-02 21:57:49 -0700

    Cleanup helper

--- helper.js
+++ helper.js
@@ -1,23 +1,10 @@
-process.nextTick(() => {
-  require('safeguards').noSynchronousIO(require.main)
-})
-
-require('trycatch').configure({'long-stack-traces': true})
+require('trycatch').configure({ 'long-stack-traces': true })
 require('songbird')
-require('safeguards')
-
-let co = require('co')
-
-process.on('uncaughtException', logError)
-
-process.on('uncaughtApplicationException', logError)
-
-process.on('unhandledRejection', logError)
 
 function logError(err) {
   console.log(err.stack)
 }
 
-process.nextTick(() => co(module.parent.exports()))
-
-// TODO: Delete all core non-promise APIs
+process.on('uncaughtException', logError)
+process.on('uncaughtApplicationException', logError)
+process.on('unhandledRejection', logError)
